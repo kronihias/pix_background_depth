4c24e78356ec4116462afd98a7c74cc7f6e73f69
Matthias Kronlachner <m.kronlachner@gmail.com>
2013-02-09 18:42:06 +0100

    initial commit

---
+++ pix_background_depth.cpp
@@ -0,0 +1,128 @@
+////////////////////////////////////////////////////////
+//
+// GEM - Graphics Environment for Multimedia
+//
+// zmoelnig@iem.kug.ac.at
+//
+// Implementation file
+//
+//    Copyright (c) 1997-1998 Mark Danks.
+//    Copyright (c) Günther Geiger.
+//    Copyright (c) 2001-2011 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
+//    For information on usage and redistribution, and for a DISCLAIMER OF ALL
+//    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+//
+/////////////////////////////////////////////////////////
+
+
+#include "pix_background_depth.h"
+#include <stdio.h>
+#include <math.h>
+
+CPPEXTERN_NEW_WITH_ONE_ARG(pix_background_depth,t_floatarg, A_DEFFLOAT);
+
+/////////////////////////////////////////////////////////
+//
+// pix_background_depth
+//
+/////////////////////////////////////////////////////////
+// Constructor
+//
+/////////////////////////////////////////////////////////
+pix_background_depth :: pix_background_depth(t_floatarg f) : m_setref(true), m_mode(false)
+{
+    if (f == 0) {
+        m_thresh = 100;
+    } else {
+        m_thresh = (int) f;
+    }
+    // inlet threshold
+    inlet_new(this->x_obj, &this->x_obj->ob_pd, gensym("float"), gensym("thresh"));
+}
+
+/////////////////////////////////////////////////////////
+// Destructor
+//
+/////////////////////////////////////////////////////////
+pix_background_depth :: ~pix_background_depth()
+{ }
+
+/////////////////////////////////////////////////////////
+// processImage
+//
+/////////////////////////////////////////////////////////
+void pix_background_depth :: processRGBAImage(imageStruct &image)
+{
+    int datasize = image.xsize * image.ysize;
+    int i;
+    
+    if ( m_setref ){
+        buffer.clear();
+    }
+    
+    unsigned char *base = image.data;
+    
+    int value = 0;
+    int userid = 0;
+	
+	for(i=0; i<datasize ; i++)    {
+		value = ((int)base[chRed] << 8) + (int)base[chGreen];
+        
+        if ( m_setref ){
+            buffer.push_back(value); // set value for subtraction
+        }
+        
+        if (!m_mode) { // different threshold computations
+            if ( value > (buffer[i] - m_thresh) ) {
+                base[chAlpha] = 0; // just set alpha zero
+            }
+        } else {
+            if ( abs(value - buffer[i]) < abs(m_thresh) ) {
+                base[chAlpha] = 0; // just set alpha zero
+            }
+        }
+        
+		base += 4;
+    }
+    
+    if ( m_setref ){
+        m_setref = false;
+    }
+}
+
+
+void pix_background_depth :: setrefMess(void *data)
+{
+    // copy current depth map to buf
+    GetMyClass(data)->m_setref = true;
+}
+
+void pix_background_depth :: threshMess(void *data, float value)
+{
+    GetMyClass(data)->m_thresh = (int) value;
+}
+
+void pix_background_depth :: modeMess(void *data, float value)
+{
+    if (value < 0.5) {
+        GetMyClass(data)->m_mode=false;
+    } else {
+        GetMyClass(data)->m_mode=true;
+    }
+}
+
+
+/////////////////////////////////////////////////////////
+// static member function
+//
+/////////////////////////////////////////////////////////
+void pix_background_depth :: obj_setupCallback(t_class *classPtr)
+{
+    class_addbang(classPtr, reinterpret_cast<t_method>(&pix_background_depth::setrefMess));
+    class_addmethod(classPtr, reinterpret_cast<t_method>(&pix_background_depth::setrefMess),
+                    gensym("setref"), A_FLOAT, A_NULL);
+    class_addmethod(classPtr, reinterpret_cast<t_method>(&pix_background_depth::threshMess),
+                    gensym("thresh"), A_FLOAT, A_NULL);
+    class_addmethod(classPtr, reinterpret_cast<t_method>(&pix_background_depth::modeMess),
+                    gensym("mode"), A_FLOAT, A_NULL);
+}
---
+++ pix_background_depth.h
@@ -0,0 +1,77 @@
+/*-----------------------------------------------------------------
+LOG
+GEM - Graphics Environment for Multimedia
+
+Clamp pixel values to a threshold
+
+Copyright (c) 1997-1998 Mark Danks. mark@danks.org
+Copyright (c) Günther Geiger. geiger@epy.co.at
+Copyright (c) 2001-2011 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
+Copyright (c) 2002 James Tittle & Chris Clepper
+For information on usage and redistribution, and for a DISCLAIMER OF ALL
+WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+-----------------------------------------------------------------*/
+
+#ifndef _INCLUDE__GEM_PIXES_pix_background_depth_H_
+#define _INCLUDE__GEM_PIXES_pix_background_depth_H_
+
+#include "Base/GemPixObj.h"
+#include <RTE/MessageCallbacks.h>
+
+
+/*-----------------------------------------------------------------
+-------------------------------------------------------------------
+CLASS
+    pix_background_depth
+    
+    Clamp pixel values to a threshold
+
+KEYWORDS
+    pix
+    
+DESCRIPTION
+
+    Subtract a ref depth map to a depth stream from a Kinect sensor
+   
+-----------------------------------------------------------------*/
+#ifdef _WIN32
+class GEM_EXPORT pix_background_depth : public GemPixObj
+#else
+class GEM_EXTERN pix_background_depth : public GemPixObj
+#endif
+{
+    CPPEXTERN_HEADER(pix_background_depth, GemPixObj);
+
+    public:
+
+        //////////
+        // Constructor
+    	pix_background_depth(t_floatarg f);
+    	
+    protected:
+    	
+    	//////////
+    	// Destructor
+    	virtual ~pix_background_depth();
+
+    	//////////
+    	// Do the processing
+    	virtual void 	processRGBAImage(imageStruct &image);
+    	
+
+    	//////////
+
+    	//////////
+    	// Set the new threshold value
+    	static void    	setrefMess(void *data);
+    	static void	    threshMess(void *data, float value);
+        static void	    modeMess(void *data, float value);
+
+		int m_thresh;
+		bool m_setref;
+        bool m_mode;
+		std::vector<int> buffer;
+};
+
+#endif	// for header file
